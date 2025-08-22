e61ec09001dcc74cd48b0f827b93b11f89117354
Christopher Graßl <105931305+chris98gr@users.noreply.github.com>
2025-08-22 17:21:56 +0200

    Add files via upload

---
+++ OTP-gqlClient.py
@@ -0,0 +1,161 @@
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+from datetime import datetime
+from datetime import timedelta
+import pandas as pd
+
+#########################################################################################
+#OTP Host IP and Port
+OTP_HOST = "localhost:8080"         #IP and Port of the OTP instance
+
+
+#Parameters for the OTP queries
+DATE_STR = "2025-06-28"             #Date of the earliest departure time (format: "YYYY-MM-DD")
+
+TIMEZONE_STR = "+02:00"             #Timezone (format: "+hh:mm")
+
+ED_START_STR = "10:00:00"           #Start of the time window for earliest departure time (format: "hh:mm:ss")
+
+ED_END_STR = "16:00:00"             #End of the time window for earliest departure time (format: "hh:mm:ss")
+                                    #set this to the same as ED_START_STR to only get routes for that specific earliest departure time
+
+STEPSIZE = 360                      #Stepsize for the earliest departure time (in minutes)
+
+SEARCH_WINDOW = "PT360M"            #Search window for OTP-routing (format: "PTxHyMzS" where x -> duration in hours, y -> duration in minutes, z -> duration in seconds)
+
+#for a more precise description of the date and time formats, check the ISO 8601 standard
+
+#Names of the workfiles and the csv with the results
+DESTINATIONS = "sportpl_dests.csv"          #csv file with the destination coordinates for the analysis (needs to be in the "workfiles" directory)
+ORIGINS = "origins.csv"                     #csv file with the origin coordinates for the analysis (needs to be in the "workfiles" directory)
+CONNECTIONS = "sportpl_connections.csv"     #name of the csv file to store the results in (will be saved in the "results" directory)
+
+#########################################################################################
+
+def plan_conn_query(origin_lon, origin_lat, dest_lon, dest_lat, departure_time):
+
+    query_str =f"""
+            query {{
+                planConnection(
+                    destination: {{
+                        location: {{
+                            coordinate: {{
+                                latitude: {dest_lat},
+                                longitude: {dest_lon}
+                            }}
+                        }}
+                    }}
+                    origin: {{
+                        location: {{
+                            coordinate: {{
+                                latitude: {origin_lat},
+                                longitude: {origin_lon}
+                            }}
+                        }}
+                    }}
+                    dateTime: {{ earliestDeparture: "{DATE_STR}T{departure_time}{TIMEZONE_STR}" }}
+                    searchWindow: "{SEARCH_WINDOW}"
+                    first: 1
+                ){{
+                    edges {{
+                        node {{
+                            start
+                            end
+                        }}
+                    }}
+                    routingErrors{{
+                        code
+                        description
+                        inputField
+                    }}
+                }}
+            }}
+        """
+
+
+    query = gql(query_str)
+
+
+    return query
+
+
+#read origins and destinations from csv
+dest_df = pd.read_csv('workfiles/' + DESTINATIONS)
+origins_df = pd.read_csv('workfiles/' +ORIGINS)
+
+#create gql transport and client
+transport = RequestsHTTPTransport(url="http://" + OTP_HOST + "/otp/gtfs/v1",
+                                  verify=False,
+                                  retries=1)
+
+client = Client(transport=transport, fetch_schema_from_transport=True)
+
+#empty dataframe to put connection data
+connections_df = pd.DataFrame()
+
+#otp debug data
+routing_errors = pd.DataFrame()
+
+#set end of time window for departure times and stepsize
+ed_end = datetime.strptime(ED_END_STR,"%H:%M:%S")
+if STEPSIZE < 1:
+    step_size = timedelta(minutes=1)
+else:
+    step_size = timedelta(minutes=STEPSIZE)
+
+#TODO add progress messages
+#loops for origins and destinations
+for origin_row in origins_df.itertuples():
+    for dest_row in dest_df.itertuples():
+
+        dep_time = datetime.strptime(ED_START_STR,"%H:%M:%S")
+
+        #loop for different departure times
+        while dep_time <= ed_end:
+            #create and send graphQL query
+            query = plan_conn_query(origin_row.X, origin_row.Y, dest_row.X, dest_row.Y, datetime.strftime(dep_time, "%H:%M:%S"))
+            
+            response = client.execute(query)
+            
+            #process response
+            if bool(response['planConnection']['edges']):
+                start_time = datetime.strptime(response['planConnection']['edges'][0]['node']['start'], '%Y-%m-%dT%H:%M:%S%z')
+                end_time = datetime.strptime(response['planConnection']['edges'][0]['node']['end'], '%Y-%m-%dT%H:%M:%S%z')
+                travel_time = end_time - start_time
+                travel_time_sec = travel_time.total_seconds()
+
+                new_row = pd.DataFrame({'origin_id': [origin_row.fid],
+                                        'dest_id': [dest_row.fid],
+                                        'dep_time': [datetime.strftime(start_time, '%Y-%m-%dT%H:%M:%S%z')],
+                                        'arr_time': [datetime.strftime(end_time, '%Y-%m-%dT%H:%M:%S%z')],
+                                        'travel_time': [travel_time_sec/60]})
+
+                connections_df = pd.concat([connections_df, new_row], ignore_index=True)
+
+            elif bool(response['planConnection']['routingErrors']):
+                new_row = pd.DataFrame({'origin_id': [origin_row.fid],
+                                        'dest_id': [dest_row.fid],
+                                        'error': [response['planConnection']['routingErrors'][0]['code']],
+                                        'description': [response['planConnection']['routingErrors'][0]['description']]})
+                routing_errors = pd.concat([routing_errors, new_row], ignore_index=True)
+
+            else:
+                new_row = pd.DataFrame({'origin_id': [origin_row.fid],
+                                        'dest_id': [dest_row.fid],
+                                        'error': ["UNKNOWN"],
+                                        'description': ["The routing result is empty, but there is no errors..."]})
+                routing_errors = pd.concat([routing_errors, new_row], ignore_index=True)
+
+
+
+            dep_time += step_size
+
+    print("Finished origin " + str(origin_row.fid))
+
+
+#save results and error-log
+connections_df.to_csv('results/' + CONNECTIONS, index=False)
+routing_errors.to_csv('results/' + CONNECTIONS + '_errors.csv', index=False)
+
+print("OTP-calculations finished.")
+
--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# OpenTripPlanner-travel-time-matrix
-A script to calculate a travel-time matrix (OD-matrix) using OpenTripPlanner
+# Opentripplanner 2.7.0 travel-time matrix (OD matrix)
+
+This repository provides a script to automatically calculate a travel time matrix
+for a set of origin and destination points, using OpenTripPlanner. 
+The resulting travel time matrix can also be turned into a typical OD matrix, by restructuring the data. 
+
+The script can be used to calculate the matrix for a single starting time of the day or several times within a specified time frame of the day.
+
+
+# OpenTripPlanner Instance
+
+The script uses an OpenTripPlanner Instance to calculate the travel-time matrix. It was developed and tested 
+using a Docker Container running OpenTripPlanner (version 2.7.0). (Assuming there's no changes to the GraphQL API 
+it should work with later versions as well)
+
+Information on how to set up and run a local Instance of OpenTripPlanner can be found at https://docs.opentripplanner.org/en/v2.7.0/Basic-Tutorial/
+
+
+# Using the script
+
+The script reads origin and destination points from separate csv files in the workfiles directory. 
+These files should contain the X (longitude) and Y (latitude) coordinate of the points in separate columns as well as 
+a fid-column containing a unique ID for each point. (Two examples are provided within the workfiles
+directory)
+
+Before starting the script, the parameters for the calculations need to be set within the
+OTP-gqlClient.py file.
+
+1. OTP Host information:
+
+    The OTP-HOST variable needs to be set to the IP-Address and Port of the OpenTripPlanner Instance as a String.
+    (e.g. if Opentripplanner is set up locally as described in the Tutorial mentioned above, it should be
+    set to "localhost:8080")
+
+2. OTP Parameters:
+
+    For the calculations, the script also needs several parameters for the OTP-queries to set the 
+    departure time(s) and search window for the routing requests:
+
+   1. DATE_STR: A string representing the Date for the calculations in the format "YYYY-MM-DD" 
+   2. TIMEZONE_STR: A string representing the timezone of the study area in the format "+hh:mm"
+   3. ED_START_STR: A string representing the start of the time frame for the earliest departure times.
+        If only a single departure time per origin/destination pair should be used, the variable should be set to that value. Format: "hh:mm:ss"
+   4. ED_END_STR: A string representing the end of the time frame for the earliest departure times.
+        If only a single departure time per origin/destination pair should be used, this value should be the same as ED_START_STR. Format: "hh:mm:ss"
+   5. STEPSIZE: The stepsize for the earliest departure time in minutes. If set to 10, for example, the script will
+        calculate routes for ED_START_STR, ED_START_STR+10min, ED_START_STR+20min, ... until reaching ED_END_STR
+        (if only a single departure time should be used, this variable can be ignored)
+   6. SEARCH_WINDOW: A string representing the search window for the OTP-queries. (duration between earliest-departure-time and latest-departure-time) 
+
+3. Workfiles and results-file:
+
+    The origins and destinations for the calculations are read from separate csv files. Both files should be placed in the workfiles directory. The names
+    of the origin and destination file, as well as the name of the file containing the results, need to be provided in the following parameters:
+
+   1. DESTINATIONS: The name of the csv file containing the destinations
+   2. ORIGINS: The name of the csv file containing the origins
+   3. CONNECTIONS: Name of the file to put the results in. This file will be stored within the results directory when the script has finished.
+
+
+After setting all the parameters and setting up an OpenTripPlanner instance, the OTP-gqlClient.py script can be run to start the calculations.
+To monitor the progress, the script will print progress messages to the terminal.
+
+When the calculations are done, the results can be found in the "results" directory.
+
+# Results
+
+The results of the calculations are put out as csv files in the "results" directory.
+The script creates one file containing the data for successfully calculated connections and 
+one file containing the routing errors, where OTP did not calculate a connection.
+
+1. In the results file with the successful calculations, each row represents a connection of two points.
+    It contains the following columns:
+   1. origin_id: The unique id of the origin point (fid from the origins workfile).
+   2. dest_id: The unique id of the destination point (fid from the dests workfile).
+   3. dep_time: The (actual) departure time of the connection.
+   4. arr_time: The (actual) arrival time of the connection.
+   5. travel_time: The travel time of the connection (in minutes).
+
+2. In the errors.csv file, the script stores pairs of origin and destination, where no connection
+    was returned by OTP. It contains the origin and destination IDs and some additional details, 
+    explaining why no connection was found.
